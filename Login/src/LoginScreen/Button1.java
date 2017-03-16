bad8d80ddf14488d3d7f95e338fbce0f780cae47
Sam M <smil143@lsu.edu>
2017-03-16 09:29:10 -0500

    Added LoginsScreen project and package initialised code base

---
+++ Login/src/LoginScreen/Button1.java
@@ -0,0 +1,7 @@
+package LoginScreen;
+
+/**
+ * Created by sithp on 3/16/2017.
+ */
+public class Button1 {
+}
---
+++ Login/src/LoginScreen/LoginScreen1.java
@@ -0,0 +1,7 @@
+package LoginScreen;
+
+/**
+ * Created by sithp on 3/16/2017.
+ */
+public class LoginScreen1 {
+}
