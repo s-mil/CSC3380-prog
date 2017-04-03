f6bc748f29c4537f3a65be62492f078976bef1bf
sam miller <sam miller>
2017-04-02 22:13:53 -0500

    orginzed and inited/planed NotFB.java

---
+++ NotFB.java
@@ -0,0 +1,13 @@
+// Sam Miller
+// 4/7/17
+// Making LSUBook A terminal based social network
+
+import java.util.Scanner;
+
+class NotFB{
+  public static void main(String[] args){
+      
+
+
+  }
+ }
